6e4ec1c437914768abf914cd0285d2a64b4e080b
Temba Sydney Gabriel <tembasydney@gmail.com>
2024-07-14 21:41:00 +0300

    lab done

--- server/app.py
+++ server/app.py
@@ -6,7 +6,7 @@ from flask_restful import Api, Resource
 
 from models import db, Plant
 
-app = Flask(__name__)
+app = Flask(__name__)  # Correct __name__
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
@@ -16,9 +16,7 @@ db.init_app(app)
 
 api = Api(app)
 
-
 class Plants(Resource):
-
     def get(self):
         plants = [plant.to_dict() for plant in Plant.query.all()]
         return make_response(jsonify(plants), 200)
@@ -37,19 +35,28 @@ class Plants(Resource):
 
         return make_response(new_plant.to_dict(), 201)
 
-
 api.add_resource(Plants, '/plants')
 
-
 class PlantByID(Resource):
-
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
-
+    
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        for attr in request.json:
+            setattr(plant, attr, request.json[attr])
+        db.session.add(plant)
+        db.session.commit()
+        return make_response(plant.to_dict(), 200)
+    
+    def delete(self, id):
+        plant = Plant.query.filter(Plant.id == id).first()
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response('', 204)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
-
-if __name__ == '__main__':
+if __name__ == '__main__':  # Correct __name__
     app.run(port=5555, debug=True)
